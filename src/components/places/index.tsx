4ac43dc800fb7b76d1b915d5950a52ef04efb597
lacerdaaa <edulacerdaaa@gmail.com>
2024-12-12 17:50:26 -0300

    -update: places list

---
+++ src/components/places/index.tsx
@@ -0,0 +1,15 @@
+import { View } from 'react-native';
+import { s } from './styles';
+import { PlaceProps } from '../place';
+
+type Props = {
+    data: PlaceProps[]
+}
+
+export default function Places({data}: Props) {
+ return (
+   <View>
+
+   </View>
+  );
+}
