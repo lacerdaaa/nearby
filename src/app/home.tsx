aa0ad19c1a443e46641577fb7ff435f3fc1fed87
lacerdaaa <edulacerdaaa@gmail.com>
2024-12-12 16:41:01 -0300

    -update: categories list

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,9 +1,31 @@
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+
+import { api } from '@/services/api';
+import { useEffect, useState } from 'react';
+import {Categories, CategoriesProps }from '@/components/categories';
 
 export default function Home() {
+
+  const [categories, setCategories] = useState<CategoriesProps>([]);
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories")
+      setCategories(data)
+    } catch (error) {
+      console.error(error)
+      Alert.alert('Erro:', 'Error in fetch categories')
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
  return (
    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-    <Text>Ol'a mundo!</Text>
+    <Categories data={categories}/>
+    
    </View>
   );
 }
---
+++ src/components/categorie/index.tsx
@@ -0,0 +1,20 @@
+import { Pressable, Text, PressableProps } from "react-native";
+
+import { s } from "./style";
+import { categoriesIcons } from "@/utils/categories-icons";
+
+type Props = PressableProps & {
+    iconId: string,
+    isSelected?: boolean,
+    name: string,
+}
+
+export function Category({name, iconId, isSelected = false, ...rest}:Props) {
+    const Icon = categoriesIcons[iconId]
+     return (
+        <Pressable style={[s.container]}>
+            <Icon size={16}/>
+            <Text style={[s.name]}>{name}</Text>
+        </Pressable>
+     )
+}
