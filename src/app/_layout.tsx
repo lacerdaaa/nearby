5419e44b432685e5ba231adfda36d1be405e3f5f
lacerda <edulacerdaaa@gmail.com>
2024-12-13 23:02:29 -0300

    -update: gesture handler and fetch markets data

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import { colors } from "@/styles/theme";
-
+import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler'
 import { useFonts, Rubik_400Regular, Rubik_500Medium, Rubik_600SemiBold,Rubik_700Bold } from '@expo-google-fonts/rubik'
 
 import Loading from "@/components/loaded";
@@ -18,11 +18,13 @@ export default function Layout() {
     }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.gray[100] },
-      }}
-    />
+    <GestureHandlerRootView style={{flex:1}}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: { backgroundColor: colors.gray[100] },
+        }}
+      />
+    </GestureHandlerRootView>
   );
 }
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,10 +3,17 @@ import { View, Text, Alert } from "react-native";
 import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Categories, CategoriesProps } from "@/components/categories";
+import { PlaceProps } from "@/components/place";
+import Places from "@/components/places";
+
+type MarketProps = PlaceProps & {}
 
 export default function Home() {
+
   const [categories, setCategories] = useState<CategoriesProps>([]);
   const [category, setCategory] = useState<string>("");
+  const [markets, setMarkets] = useState<MarketProps[]>([])
+
 
   async function fetchCategories() {
     try {
@@ -19,9 +26,27 @@ export default function Home() {
     }
   }
 
+  async function fetchMarket() {
+    try {
+      if(!category) {
+        return 
+      }
+      const { data } = await api.get('/markets/category/' + category);
+      setMarkets(data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      Alert.alert("ERRO DE FETCH", " Was not possible to fetch market data");
+    }
+  }
+
   useEffect(() => {
     fetchCategories();
   }, []);
+
+  useEffect(() => {
+    fetchMarket();
+  }, [category]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
@@ -30,6 +55,7 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
+      <Places data={markets}/>
     </View>
   );
 }
